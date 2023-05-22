5344bb5306ddfd6480664a1649c668ed3de8766d
Franklin Delehelle <franklin.delehelle@odena.eu>
2023-05-22 12:40:35 +0200

    chore: add git-cliff to dependencies

--- shell.nix
+++ shell.nix
@@ -3,6 +3,6 @@
 pkgs.mkShell {
   buildInputs = [
     pkgs.cargo pkgs.rust-analyzer pkgs.rustc pkgs.rustfmt pkgs.clippy
-    pkgs.sqlite pkgs.libiconv
+    pkgs.sqlite pkgs.libiconv pkgs.git-cliff
   ];
 }
